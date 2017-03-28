bbed9f3b3f96736419ec0d8c5cfb25af66ad93bf
Chủ Gấu <Ch Gu>
2017-03-28 21:15:32 +0700

    sửa login

--- Login/index.php
+++ Login/index.php
@@ -116,7 +116,7 @@
                     </div>
                     <div class="col-md-5">
                         <div class="icon-facebook">
-                            <a href="https://www.facebook.com/dialog/oauth?client_id=536643483177466&redirect_uri=http://localhost/test/fblogin.php&scope=public_profile" target="blank">
+                            <a href="https://www.facebook.com/dialog/oauth?client_id=536643483177466&redirect_uri=http://localhost:8080/betez/login/fblogin.php&scope=public_profile" target="blank">
                                 <img alt="Login with Facebook" src="http://i.imgur.com/959OX3J.png" />
                             </a>
                         </div>
