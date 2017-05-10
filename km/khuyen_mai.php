f5f44ce40a28acc0ea506f1c6eb8b49df93f82da
ptquang97 <ptquang97@gmail.com>
2017-05-10 21:38:09 +0700

    Them trang KM

---
+++ km/khuyen_mai.php
@@ -0,0 +1,75 @@
+<!DOCTYPE htmlxmlns="http://www.w3.org/1999/xhtml">
+<html>
+<head>
+<title>Khuyến mại</title>
+<meta name="Khuyenmai" content="text/html"; charset="utf-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+<link href="https://fonts.googleapis.com/css?family=Anton|Audiowide|Baloo|Chewy|Cookie|Fjalla+One|Gloria+Hallelujah|Kaushan+Script|Lobster|PT+Sans+Narrow|Pacifico|Righteous"
+rel="stylesheet">
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/cart.js"></script>
+<script src="js/snow.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+<link href="style.css" type="text/css" rel="stylesheet">
+
+</head>
+<body>
+<?php
+        //  session_start();
+    ?>
+        <?php
+         require_once('modules/header.php');
+    ?>
+<div class='main-container'>
+    <div class='main'>
+        <div class="cate">
+            <div class="header">
+                <h2>Tin mới và ưu đãi</h2>
+            </div>
+                
+        </div>
+        
+    </div>
+    
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-8">
+            <div class="carousel slide" id="carousel-813004">
+                <ol class="carousel-indicators">
+                    <li data-slide-to="0" data-target="#carousel-813004">
+                    </li>
+                    <li data-slide-to="1" data-target="#carousel-813004">
+                    </li>
+                    <li data-slide-to="2" data-target="#carousel-813004" class="active">
+                    </li>
+                </ol>
+                <div class="carousel-inner">
+                    
+                    <div class="item">
+                        <img alt="Carousel Bootstrap Second" src="images/vl.png" class="banner" />
+                        </div>
+                    <div class="item">
+                        <img alt="Carousel Bootstrap Second" src="images/asd.png" class="banner"/>
+                        </div>
+                    <div class="item active">
+                        <img alt="Carousel Bootstrap Third" src="images/sua.png" class="banner" />
+                        
+                    </div>
+                </div> 
+
+                <a class="left carousel-control" href="#carousel-813004" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-813004" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <div class="col-md-2">
+
+        </div>
+    </div>
+</div>
+        <script src="http:///code.jquery.com/jquery.js"></script>
+        <script src="http:///netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
